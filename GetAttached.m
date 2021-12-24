681a372f02ab36fa0df71e9a5b810946ccc4e186
PBeamA <PBeamA@users.noreply.github.com>
2021-12-24 13:55:19 +1300

    Push!

---
+++ GetAttached.m
@@ -0,0 +1,108 @@
+function GetAttached
+close all
+clear all
+clc
+tic
+
+filename = 'BKMGC.tex';
+
+fid = fopen(filename, 'rt');
+
+STRING = fread(fid, '*char')';
+
+fclose(fid);
+
+
+% STRING
+%
+% EXT = {'.pdf', '.csv', '.m', '.tex', '.eps', '.png', '.jpg'};
+EXT = {'.pdf', '.eps', '.png', '.jpg'};
+
+EXT = EXT(1);
+
+EXT = regexprep(EXT, '\.', '\\.');
+% STROMG
+FILESstring = '';
+for i = 1 : length(EXT)
+    ext = EXT{i};
+    
+    
+    dFILES = regexprep(STRING, sprintf('[^"]*"([^"]+)"%s[^"]*', ext), sprintf('$1%s\n', ext));
+    % %     dFILES = regexprep(STRING, sprintf('[^"]*{([^"]+)"%s}[^"]*', ext), sprintf('$1\n'))
+    %
+    % regexprep(STRING, sprintf('[^"]*{([^"]+)"%s}[^"]*', ext), sprintf('$1\n'))
+    
+    %     asd
+    if ~strcmp(STRING, dFILES)
+        FILESstring = [FILESstring, dFILES];
+    else
+        %         ext
+    end
+    
+end
+
+LOC = strfind([sprintf('\n'), FILESstring], sprintf('\n'));
+
+nLOC = length(LOC);
+
+FILES = cell(nLOC - 1, 1);
+for i = 1 : nLOC - 1
+    FILES{i} = FILESstring(LOC(i) : LOC(i + 1) - 2);
+end
+% FILES
+
+nFILES = length(FILES);
+% nFILES = 1
+% Folders = [];
+FullFilename = cell(nFILES, 1);
+for i = 1 : nFILES
+    
+    [~, fNAME, fEXT] = fileparts(FILES{i});
+    %     regexprep(FILES{i}, '\.[^\.]+', '')
+    
+    
+    % regexprep(STRING, sprintf('.*{([^\"\{]*)"%s"%s}.*', fNAME, fEXT), '$1')
+    Folder = regexprep(STRING, sprintf('.*{([a-zA-Z /]+)"%s"%s}.*', fNAME, fEXT), '$1');
+    FullFilename{i, 1} = [Folder, FILES{i}];
+end
+
+
+FOLDERS = unique(regexprep(FullFilename, '/[^/]+\.[^\.]+', ''));
+
+for i = 1 : length(FOLDERS)
+    
+    if ~isfolder(['USED/' FOLDERS{i}])
+        mkdir(['USED/' FOLDERS{i}])
+    end
+end
+for i = 1 : nFILES
+    %     ['USED/', FullFilename{i}]
+    copyfile(FullFilename{i}, ['USED/', FullFilename{i}])
+    
+    
+    
+end
+
+
+toc
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ GetBib.m
@@ -0,0 +1,122 @@
+function GetBib
+close all
+clear all
+clc
+
+
+filename = 'BKMAJDFull';
+
+
+fid = fopen([filename, '.bcf'], 'rt');
+
+STRING = fread(fid, '*char')';
+
+fclose(fid);
+% STRING
+
+
+STRING = regexprep(STRING, '.*(<bcf:citekey order="1">)', '$1');
+STRING = regexprep(STRING, '</bcf:section>.*', '');
+STRING = regexprep(STRING, '[ ]*', '');
+STRING = regexprep(STRING, '<[^<^>]*>', '');
+
+% regexprep(STRING, '\n.*', '')
+
+KEYS = [];
+while length(STRING) > 1
+    KEYS{end + 1, 1} = regexprep(STRING, '\n.*', '');
+    STRING = regexprep(STRING, regexprep(STRING, '\n.*', ''), '');
+    STRING(1) = [];
+    STRING = regexprep(STRING, '\s*', '\n');
+%     STRING = regexprep(STRING, '\r\n\r\n', '\r\n')
+
+
+
+
+% STRING = regexprep(STRING, '\n(.*)', '$1');
+% asdasd
+
+%     pause
+end
+
+
+KEYS = unique(KEYS);
+
+
+if length(KEYS{1}) == 0
+    KEYS(1) = [];
+   KEYS;
+end
+
+CitedKeys = KEYS;
+length(KEYS)
+
+
+
+
+
+
+
+
+
+fid = fopen([filename, '.bib'], 'rt');
+
+STRING = fread(fid, '*char')';
+
+fclose(fid);
+
+fid = fopen([filename, '_CLEAN.bib'], 'wt');
+BIBKEYS = regexprep(STRING, '@[\w]+{([^,]+),[^@]+', '$1\n');
+
+nCitedKeys = length(CitedKeys);
+% nCitedKeys = 1;
+for i = 1 : nCitedKeys
+    
+    
+bib = regexprep(STRING, sprintf('.*(@[\\w]+{%s,[^@]+).*', CitedKeys{i}), '$1');
+
+bib = regexprep(bib, '\\', '\\\\');
+
+bib = regexprep(bib, '%', '%%');
+    fprintf(fid, bib);
+    
+%     bib
+    
+    
+%     pause
+end
+fclose(fid);
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
